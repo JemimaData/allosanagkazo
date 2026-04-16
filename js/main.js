29e79f065a5c798c1f214bb3cbc515c791f5d68a
NONVIGNON  IBITAYO JEMIMA <jemimanonvignon@gmail.com>
2026-04-16 11:40:58 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -104,3 +104,25 @@ gallery.addEventListener("mouseenter", () => {
 gallery.addEventListener("mouseleave", () => {
   scrollSpeed = 1;
 });
+
+const images = document.querySelectorAll('.g-img');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+const closeBtn = document.querySelector('.close');
+
+images.forEach(img => {
+  img.addEventListener('click', () => {
+    lightbox.style.display = 'flex';
+    lightboxImg.src = img.src;
+  });
+});
+
+closeBtn.addEventListener('click', () => {
+  lightbox.style.display = 'none';
+});
+
+lightbox.addEventListener('click', (e) => {
+  if (e.target === lightbox) {
+    lightbox.style.display = 'none';
+  }
+});
